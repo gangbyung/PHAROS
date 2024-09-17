0dbefad88d0541f13ec388c5705f73d683332962
강병구 <a01066420995@gmail.com>
2024-09-17 23:01:09 +0900

    anim

--- Assets/1.Script/AI/AI_Rotation.cs
+++ Assets/1.Script/AI/AI_Rotation.cs
@@ -19,22 +19,49 @@ public class AI_Rotation : MonoBehaviour
         if (direction == transform.TransformDirection(Vector3.forward))
         {
             Debug.Log("앞으로 회전");
-            avatarAnimator.SetTrigger("Idle");  // Idle 상태를 기본으로 유지
+            StartCoroutine(Animcor());
+            //avatarAnimator.SetTrigger("Idle");  // Idle 상태를 기본으로 유지
         }
         else if (direction == transform.TransformDirection(Vector3.back))
         {
-            Debug.Log("뒤로 회전");
-            avatarAnimator.SetTrigger("Turn180");  // 180도 회전 애니메이션
+            Debug.Log("뒤로 회전");  // 180도 회전 애니메이션
+            StartCoroutine(AnimcorBack());
         }
         else if (direction == transform.TransformDirection(Vector3.left))
         {
             Debug.Log("왼쪽으로 회전");
-            avatarAnimator.SetTrigger("Turn90 Left");  // 90도 왼쪽 회전
+            StartCoroutine(AnimcorLeft());
+            // 90도 왼쪽 회전
         }
         else if (direction == transform.TransformDirection(Vector3.right))
         {
             Debug.Log("오른쪽으로 회전");
-            avatarAnimator.SetTrigger("Turn90 Right");  // 90도 오른쪽 회전
+            StartCoroutine(AnimcorRight());
+            // 90도 오른쪽 회전
         }
+    }
+    IEnumerator Animcor()
+    {
+        yield return new WaitForSeconds(0.2f);
+        avatarAnimator.SetTrigger("Idle");  // Idle 상태를 기본으로 유지
+        
+    }
+    IEnumerator AnimcorLeft()
+    {
+        yield return new WaitForSeconds(0.2f);
+        avatarAnimator.SetTrigger("Turn90 Left");  // Idle 상태를 기본으로 유지
+
+    }
+    IEnumerator AnimcorRight()
+    {
+        yield return new WaitForSeconds(0.2f);
+        avatarAnimator.SetTrigger("Turn90 Right");  // Idle 상태를 기본으로 유지
+
+    }
+    IEnumerator AnimcorBack()
+    {
+        yield return new WaitForSeconds(0.2f);
+        avatarAnimator.SetTrigger("Turn180");  // Idle 상태를 기본으로 유지
+
     }
 }
